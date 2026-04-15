bb954fc97c7a646408a839244cc03995538c319d
IGNACIO URIBE <i.uribe01@ufromail.cl>
2026-04-15 10:05:10 -0400

    first commit

---
+++ src/main/java/pruebas/software/propertysbase/PropertysbaseApplication.java
@@ -0,0 +1,13 @@
+package pruebas.software.propertysbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropertysbaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropertysbaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pruebas/software/propertysbase/PropertysbaseApplicationTests.java
@@ -0,0 +1,13 @@
+package pruebas.software.propertysbase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropertysbaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
