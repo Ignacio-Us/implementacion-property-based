1728b4e0f8836e9a62b5cf15227fe28658ca2e8a
IGNACIO URIBE <i.uribe01@ufromail.cl>
2026-04-16 22:33:20 -0400

    feat(model): Creacion de la entidad usuario

--- src/main/java/pruebas/software/propertysbase/domain/model/User.java
+++ src/main/java/pruebas/software/propertysbase/domain/model/User.java
@@ -1,5 +1,31 @@
 package pruebas.software.propertysbase.domain.model;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@Builder(toBuilder = true)
+@ToString
 public class User {
+    private final Long id;
+    private final String name;
+    private final String email;
+    private final String password;
     
+    public boolean isValid() {
+        return isNameValid() && isEmailValid() && isPasswordValid();
+    }
+
+    private boolean isNameValid() {
+        return name != null && !name.trim().isEmpty();
+    }
+
+    private boolean isEmailValid() {
+        return email != null && email.contains("@") && email.length() > 5;
+    }
+
+    private boolean isPasswordValid() {
+        return password != null && password.length() >= 8;
+    }
 }
